922d6caad4fd8377c60e503aaac15f06680d80b9
RITEFANG <1721985272@QQ.COM>
2024-03-18 08:50:38 +0800

    Update BoardConfig.mk
    mondrian-->unicorn

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,9 +5,9 @@
 #
 
 # Inherit from the proprietary version
-include vendor/xiaomi/miuicamera-mondrian/BoardConfigVendor.mk
+include vendor/xiaomi/miuicamera-unicorn/BoardConfigVendor.mk
 
-MIUICAMERA_PATH := device/xiaomi/miuicamera-mondrian
+MIUICAMERA_PATH := device/xiaomi/miuicamera-unicorn
 
 # Properties
 TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
